69c4a2c8e4bacefdf0d29a0567c6869148f2797a
tiwarisrikanth <tiwarisrikanth@gmail.com>
2020-06-23 17:51:20 +0530

    Corona Virus Tracking app

---
+++ app/src/main/java/com/srikanth/android/covid_19tracker/ui/country/CovidCountryAdapter.java
@@ -0,0 +1,51 @@
+package com.srikanth.android.covid_19tracker.ui.country;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.srikanth.android.covid_19tracker.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CovidCountryAdapter extends RecyclerView.Adapter<CovidCountryAdapter.ViewHolder> {
+
+    ArrayList<CovidCountry> covidCountries;
+
+    public CovidCountryAdapter(ArrayList<CovidCountry> covidCountries) {
+        this.covidCountries = covidCountries;
+    }
+
+    @NonNull
+    @Override
+    public CovidCountryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_covid_country, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CovidCountryAdapter.ViewHolder holder, int position) {
+        CovidCountry covidCountry = covidCountries.get(position);
+        holder.tvTotalCases.setText(covidCountry.getmCases());
+        holder.tvCountryName.setText(covidCountry.getmCovidCountry());
+    }
+
+    @Override
+    public int getItemCount() {
+        return covidCountries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTotalCases, tvCountryName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTotalCases = itemView.findViewById(R.id.tvTotalCases);
+            tvCountryName = itemView.findViewById(R.id.tvCountryname);
+        }
+    }
+}
